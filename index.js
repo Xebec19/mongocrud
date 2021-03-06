0698b91aaa7dfca11744a782e7be7d2545ab62fd
xebec19 <thakur190420@gmail.com>
2021-03-06 15:17:34 +0530

    init

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import passport from 'passport'
+
 const app = express()
 const port = 3000
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//import routes
+import auth from './routes/api/auth') 
+
+//config for passport used for handling tokens
+app.use(passport.initialize());
+
+import { passport } from './strategies/jwtStrategy'
 
 app.get(
   '/',

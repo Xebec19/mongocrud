6d83041943c632b613c385653012380d7df0f6eb
xebec19 <thakur190420@gmail.com>
2021-03-07 09:05:44 +0530

    setting up jwt

---
+++ secret/myURL.js
@@ -0,0 +1,2 @@
+const secret = 'Fuegoleon'
+export default secret;
--- strategies/jwtStrategy.js
+++ strategies/jwtStrategy.js
@@ -1,5 +1,9 @@
 import {JwtStrategy,ExtractJwt} from 'passport-jwt'
 import mongoose from 'mongoose'
 import Person from '../models/Person'  //import model from person
+import secret from '../secret/myURL'
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
 
 
